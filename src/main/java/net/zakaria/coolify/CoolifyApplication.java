7bf98321f239dedeffc35d59b4492f4bc8cc9a74
Zakaria EL MOUMNAOUI <zakaria-statistics>
2025-02-04 12:02:18 +0100

    first commit

---
+++ src/main/java/net/zakaria/coolify/CoolifyApplication.java
@@ -0,0 +1,25 @@
+package net.zakaria.coolify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class CoolifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolifyApplication.class, args);
+    }
+
+}
+
+@RestController
+@RequestMapping("/")
+class HelloController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello, Coolify!";
+    }
+}
---
+++ src/test/java/net/zakaria/coolify/CoolifyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zakaria.coolify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
